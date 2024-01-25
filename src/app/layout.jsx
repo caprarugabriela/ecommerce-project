9943c4bd39686743aac18897fb5b12fdb116a5c0
caprarugabriela <121718275+caprarugabriela@users.noreply.github.com>
2024-01-25 11:10:46 +0200

    Curs 22.01.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,12 @@
-import { Work_Sans } from "next/font/google";
-// import { css } from "@emotion/css";
-import "./globals.css";
+import { Work_Sans } from 'next/font/google';
+import './globals.css';
+import { Footer, Header } from '@/components/common/server';
 
-const workSans = Work_Sans({ subsets: ["latin"] });
+const workSans = Work_Sans({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Pixellab Ecom Upsalla",
-  description: "Our first ecommerce app",
+  title: 'Pixellab Ecom Upsalla',
+  description: 'Our first ecommerce app',
 };
 
 export default function RootLayout({ children }) {
@@ -14,12 +14,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={workSans.className}>
         <div className="layout-grid">
-          <header className="header">header</header>
+          <header className="header">
+            <Header></Header>
+          </header>
 
           <div className="main-area">
             <main className="content">{children}</main>
 
-            <footer className="footer">footer</footer>
+            <footer className="footer">
+              <Footer></Footer>
+            </footer>
           </div>
         </div>
       </body>
---
+++ src/components/ui/server/Separator.jsx
@@ -0,0 +1,3 @@
+export const Separator = () => {
+  return <div className="border-t border-t-neutral-400"></div>;
+};
