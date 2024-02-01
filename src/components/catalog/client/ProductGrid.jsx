700de89360b129ef341ed0155950358d87d6b463
caprarugabriela <121718275+caprarugabriela@users.noreply.github.com>
2024-02-01 13:06:31 +0200

    curs 30.01

--- src/components/catalog/client/ProductGrid.jsx
+++ src/components/catalog/client/ProductGrid.jsx
@@ -2,10 +2,22 @@ import { useProducts } from '@/hooks';
 import { ProductTile } from './ProductTile';
 import { css } from '@emotion/css';
 import { ThreeDots } from 'react-loader-spinner';
+import { useEffect, useState } from 'react';
 
 export const ProductGrid = () => {
-  const { products, loading } = useProducts();
+  const { products, loading, error } = useProducts();
+  const [paginatedProducts, setPaginatedProducts] = useState([]);
   const itemsPerRow = 2;
+  const [perPage, setPerPage] = useState(8);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const newPaginatedProducts = products
+      .slice()
+      .splice(perPage * (page - 1), perPage);
+
+    setPaginatedProducts(newPaginatedProducts);
+  }, [products, perPage, page]);
 
   const gridCssClass = css`
     display: grid;
@@ -33,17 +45,50 @@ export const ProductGrid = () => {
     );
   }
 
-  return (
-    <ul className={gridCssClass}>
-      {products.map((product) => {
-        const { id } = product;
+  if (error.trim().length > 0) {
+    return <div className="container mx-auto px-4">{error}</div>;
+  }
 
-        return (
-          <li key={id}>
-            <ProductTile product={product}></ProductTile>
-          </li>
-        );
-      })}
-    </ul>
+  const pageCount = Math.ceil(products.length / perPage);
+
+  return (
+    <>
+      <ul className={gridCssClass}>
+        {paginatedProducts.map((product) => {
+          const { id } = product;
+
+          return (
+            <li key={id}>
+              <ProductTile product={product}></ProductTile>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="flex gap-2">
+        {Array(pageCount)
+          .fill(' ')
+          .map((_, index) => {
+            const pageIndex = index + 1;
+
+            return (
+              <li key={index}>
+                <button
+                  type="button"
+                  title={`Page ${pageIndex}`}
+                  className={`border border-zinc-200 p-2 hover:bg-black hover:text-white transition-colors ${
+                    pageIndex === page ? 'bg-black text-white' : ''
+                  }`}
+                  onClick={() => {
+                    setPage(pageIndex);
+                  }}
+                >
+                  {pageIndex}
+                </button>
+              </li>
+            );
+          })}
+      </ul>
+    </>
   );
 };
